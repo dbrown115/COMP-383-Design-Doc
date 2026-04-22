31361a4ac33e9ca77b4b336c4b64b5815d9e86d7
amathew6 <141669827+amathew6@users.noreply.github.com>
2026-04-22 15:10:28 -0500

    Add files via upload

--- runanib_mtb.py
+++ runanib_mtb.py
@@ -1,25 +1,22 @@
-#before running script, one needs to open up the pyANI environment on their terminal 
-# run conda activate pyani_env in your terminal 
-#you should see (pyani_env) instead of base and that's how you know you're in the pyANI environment 
 import subprocess #used to run pyANI
 import os
 import pandas as pd #used it to visualize my data in a "table" format
 
 
-input_dir = "/home/amathew6/ANIProject/ANIb/streptococcus_3genomes" #where my genomes.fna files live
-output_dir = "/home/amathew6/ANIProject/ANIb/streptococcus_ANIb_results" #where all the files that I am generating get output
+input_dir = "/home/amathew6/ANIProject/ANIb/mtb_3genomes" #where my genomes.fna files live
+output_dir = "/home/amathew6/ANIProject/ANIb/mtb_ANIb_results" #where all the files that I am generating get output
 
 
 os.makedirs(output_dir, exist_ok=True) #when I first ran this, I didn't have an output directory made, so this command just made the directory for me
 
-print("Starting ANIb run...") #i added print statements because when I first ran this the first few times it didn't work and I wanted to see where my code was failing
+print("Starting ANIb run...") #i added print statements becauscd e when I first ran this the first few times it didn't work and I wanted to see where my code was failing
 
 
 subprocess.run([
     "average_nucleotide_identity.py", #this will find the average_nucleotide_identity that is built into the pyANI tool 
     "-i", input_dir, #calls the genomes in my input directory 
     "-o", output_dir, #outputs all the results to output_dir which leads to my mtb_3genomes_ani folder
-    "-m", "ANIb",  #shows what kind of ANIm I want to run 
+    "-m", "ANIb",  #shows what kind of ANIb I want to run 
     "--workers", "4", #uses 4 cores 
     "-g", #generates the graphs (heatmaps) for this output
     "-f" #when running this script many time -f is used to froce a manual overwrite 
@@ -27,7 +24,7 @@ subprocess.run([
 
 
 
-df = pd.read_csv("streptococcus_3genomes_ani/ANIb_percentage_identity.tab", sep="\t", index_col=0) #creates an easy readable table to interpret ANIb results 
+df = pd.read_csv("mtb_3genomes_ani/ANIb_percentage_identity.tab", sep="\t", index_col=0) #creates an easy readable table to interpret ANIb results 
 
 
 df_percent = df * 100 #did this becauase it defaulted to printing as a decimal 
